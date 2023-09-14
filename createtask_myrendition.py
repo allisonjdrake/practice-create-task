2cbf2220f929c5f5c1fd520884981754f1955836
Allison Drake <junesol.drake@gmail.com>
2023-09-14 20:21:16 +0000

    Figured out how to call function. Need to work on reuseability

--- createtask_myrendition.py
+++ createtask_myrendition.py
@@ -1,21 +1,27 @@
-colors = ["red", "orange", "yellow", "green", "blue", "purple", "white", "black", "pink"]
-styles = ["comic", "realistic", "painterly", "anime", ""]
-objects = ["basket of fruit", "woman", "man", "cats and dogs", "technology from the year 2100"]
+color = ["red", "orange", "yellow", "green", "blue", "purple", "white", "black", "pink"]
+style = ["comic", "realistic", "painterly", "anime", ""]
+object = ["basket of fruit", "woman", "man", "cats and dogs", "technology from the year 2100"]
 your_parameters = []
+my_parameters = [1,2,3,4,5,6]
+taste = ['good','bad', 'bitter', 'sweet', 'sour']
+sound = ['loud', 'quiet', 'sharp', 'mellow', 'angry']
+sight = ['fun', 'boring', 'pretty', 'nice', 'terrible']
 
-def randomization(random_idea, colorindex, styleindex, objectindex, your_parameters):
-    random_idea = input("Would you like to generate something random to draw? Yes/No")
+random_idea = input("Would you like to generate something random to draw? Yes/No")
+
+def randomization(random_idea,color,style,object,your_parameters):
     if random_idea == "Yes":
-        colorindex = int(input("Choose a number between 0 and 5 to get a random color to use!"))
-        your_parameters.append(colorindex,colors[colorindex])
-        styleindex = int(input ("Choose another number between 0 and 5 to get a random style to draw in!"))
-        your_parameters.append(styleindex, styles[styleindex])
-        objectindex = int(input("Choose a number between 0 and 5 to get a random object to draw!"))
-        your_parameters.append(objectindex, objects[objectindex])
+        i1 = int(input(f"Choose a number between 0 and 5 to get a random {color} to use!"))
+        your_parameters.insert(i1,color[i1])
+
+        i2 = int(input(f"Choose another number between 0 and 5 to get a random {style} to draw in!"))
+        your_parameters.insert(i2, style[i2])
+
+        i3 = int(input(f"Choose a number between 0 and 5 to get a random {object} to draw!"))
+        your_parameters.insert(i3, object[i3])
     else:
         print("That's fine. Maybe next time!")
-    print(f"Your random color is {your_parameters[0]}." 
-    "Your random style to draw in is {your_parameters[1]}." 
-    "Your random object to draw is {your_parameters[2]}")
-    
-print(randomization("Yes",1,2,3,your_parameters))
+    print(f"Your random {color} is {your_parameters[0]}." 
+    f"Your random {style} to draw in is {your_parameters[1]}." 
+    f"Your random {object} to draw is {your_parameters[2]}.")
+randomization(random_idea,taste,sound,sight, my_parameters)
