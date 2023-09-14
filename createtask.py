37e784332717689fffae4258f18987a558489287
Allison Drake <junesol.drake@gmail.com>
2023-09-14 20:21:51 +0000

    figured out how to call the function

--- createtask.py
+++ createtask.py
@@ -6,9 +6,7 @@ new_res = input("what restaurant would you like to rank into your list?")
 #making function with parameters makes the code "reuseable" (one of the requirements)
 def rank_restaurant(new_res, restaurants):
     for i in range (len(restaurants)):
-
         rank = input(f"do you like A) {new_res} or B) {restaurants[i]} more A/B?")
-
         if rank == "A":
             restaurants.insert(i, new_res)
             break
@@ -18,5 +16,4 @@ def rank_restaurant(new_res, restaurants):
 
     if new_res not in restaurants:
         restaurants.append(new_res)
-
-    print(f"Your new ranking of restaurants is {rank_restaurant(new_res, restaurant)}")
+print(f"Your new ranking of restaurants is {rank_restaurant(new_res, restaurants)}")
